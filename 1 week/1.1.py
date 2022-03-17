d800c3bf73db54a1368767fceda5b219e95606cb
jidream02 <101781587+jidream02@users.noreply.github.com>
2022-03-17 12:17:40 +0600

    Add files via upload

---
+++ 1 week/1.1.py
@@ -0,0 +1,7 @@
+name = input("Ваше фамилия, имя, отчество?")
+age = int (input("Сколько вам лет"))
+home = input ("Где вы живете?")
+
+print("Ваше имя",name)
+print('ваш возраст' + str(age))
+print('Вы живете', home)
