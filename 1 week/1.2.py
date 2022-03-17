9d51ef6e0fe4932a6a8d5e92a9470283aadf1933
jidream02 <101781587+jidream02@users.noreply.github.com>
2022-03-17 12:27:47 +0600

    Add files via upload

---
+++ 1 week/1.2.py
@@ -0,0 +1,6 @@
+name = input("Ваше имя, фамилия?")
+year = input("Ваша дата рождения")
+learning = input("Где вы учитесь?")
+print("Ваше имя, фамилия",name)
+print('Ваша дата рождения' + str(year))
+print ('вы учитесь в',learning)
