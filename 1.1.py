113e37c70dbcbf361cc9cc9738a4389600c89044
jidream02 <101781587+jidream02@users.noreply.github.com>
2022-03-22 19:06:32 +0600

    Delete 1.1.py

--- 1.1.py
+++
@@ -1,7 +0,0 @@
-name = input("Ваше фамилия, имя, отчество?")
-age = int (input("Сколько вам лет"))
-home = input ("Где вы живете?")
-
-print("Ваше имя",name)
-print('ваш возраст' + str(age))
-print('Вы живете', home)
